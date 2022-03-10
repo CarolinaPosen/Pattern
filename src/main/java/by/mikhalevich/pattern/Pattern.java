f1901a9760f91187450160363a9432fa36640be8
Yauheni <curiosityopportunity@gmail.com>
2022-03-10 13:04:22 +0300

    initial-commit

---
+++ src/main/java/by/mikhalevich/pattern/Pattern.java
@@ -0,0 +1,16 @@
+package by.mikhalevich.pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/pattern")
+public class Pattern extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Start Servlet!");
+    }
+}
